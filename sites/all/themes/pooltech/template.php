719cac37908a1abd3f3c4136bc6e76e511e7b174
Carlos Koch <ckoch@plaxo.com>
2015-03-25 11:40:22 -0700

    Add page node suggestion override.

--- sites/all/themes/pooltech/template.php
+++ sites/all/themes/pooltech/template.php
@@ -52,11 +52,11 @@ function STARTERKIT_preprocess_html(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+function pooltech_preprocess_page(&$variables, $hook) {
+  if (!empty($vars['node'])) {
+    $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
+  }
 }
-// */
 
 /**
  * Override or insert variables into the node templates.
